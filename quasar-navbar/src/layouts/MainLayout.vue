229fb447d72882f01191ef3fecdc4dd1bcdaf11d
Kaan Kaltakkıran <durdu.kaan.kaltakkiran@gmail.com>
2024-05-22 15:32:29 +0300

    added static navbar

--- quasar-navbar/src/layouts/MainLayout.vue
+++ quasar-navbar/src/layouts/MainLayout.vue
@@ -1,106 +1,27 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
-
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in linksList"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
-    <q-page-container>
-      <router-view />
-    </q-page-container>
-  </q-layout>
+    <q-toolbar class="bg-purple text-white shadow-2 rounded-borders">
+      <q-btn flat label="Homepage" />
+      <q-space />
+      <q-tabs v-model="tab" shrink stretch>
+        <q-tab name="tab1" label="Tab 1" />
+        <q-tab name="tab2" label="Tab 2" />
+        <q-tab name="tab3" label="Tab 3" />
+        <q-btn-dropdown auto-close flat label="More">
+          <q-list>
+            <q-item clickable @click="tab = 'tab4'">
+              <q-item-section>Tab 4</q-item-section>
+            </q-item>
+            <q-item clickable @click="tab = 'tab5'">
+              <q-item-section>Tab 5</q-item-section>
+            </q-item>
+          </q-list>
+        </q-btn-dropdown>
+      </q-tabs>
+    </q-toolbar>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import EssentialLink, { EssentialLinkProps } from 'components/EssentialLink.vue';
 
-defineOptions({
-  name: 'MainLayout'
-});
-
-const linksList: EssentialLinkProps[] = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
-
-const leftDrawerOpen = ref(false);
-
-function toggleLeftDrawer () {
-  leftDrawerOpen.value = !leftDrawerOpen.value;
-}
+const tab = ref('tab1');
 </script>
