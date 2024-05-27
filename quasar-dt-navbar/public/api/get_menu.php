383c2c9cad03f0fe85ef9f8110c28526386c83db
Kaan Kaltakkıran <durdu.kaan.kaltakkiran@gmail.com>
2024-05-27 15:27:24 +0300

    api code updated

--- quasar-dt-navbar/public/api/get_menu.php
+++ quasar-dt-navbar/public/api/get_menu.php
@@ -3,19 +3,29 @@
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 
-include 'db.php'; // Veritabanı bağlantısını sağlar
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "navbar";
 
-$sql = "SELECT * from  FROM my_web_pages";
-$result = $conn->query($sql);
+try {
+    $DB = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
+    // set the PDO error mode to exception
+    $DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //echo "Connected successfully"; // Bağlantı başarılı
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    die();
+}
+
+$sql = "SELECT * FROM my_web_pages"; // Tablo adını belirtin
+$result = $DB->query($sql);
 
 $menus = array(); //menüleri tutacak dizi
-if ($result->num_rows > 0) { // Eğer sonuçlar varsa
-    while ($row = $result->fetch_assoc()) {
+if ($result->rowCount() > 0) { // Eğer sonuçlar varsa
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
         $menus[] = $row;
     }
 }
 
 echo json_encode($menus); //json formatına çevirir
-
-$conn->close();
-?>
