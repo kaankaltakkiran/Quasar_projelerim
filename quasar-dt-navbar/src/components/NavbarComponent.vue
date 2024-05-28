b6af4a50f69b765dd0499eeb17a6033222223658
Kaan Kaltakkıran <durdu.kaan.kaltakkiran@gmail.com>
2024-05-28 13:16:45 +0300

    bug fixed mobile menu router to parameter

--- quasar-dt-navbar/src/components/NavbarComponent.vue
+++ quasar-dt-navbar/src/components/NavbarComponent.vue
@@ -51,7 +51,7 @@
         </q-item>
         <q-btn-dropdown v-for="menu in filteredMenus" :key="menu.id" auto-close stretch flat :label="menu.page_name">
             <q-list>
-              <q-item v-for="child in menu.children" :key="child.id" :to="child.page_slug" clickable @click="setTab(child.page_slug)">
+              <q-item v-for="child in menu.children" :key="child.id" :to="`/sayfalar/${child.page_slug}`" clickable @click="setTab(child.page_slug)">
                 <q-item-section>{{ child.page_name }}</q-item-section>
               </q-item>
             </q-list>
