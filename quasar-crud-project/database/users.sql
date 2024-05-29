9d89a04502f33cc77233385f2ef0eca609e049b2
Kaan Kaltakkıran <durdu.kaan.kaltakkiran@gmail.com>
2024-05-29 22:10:30 +0300

    added database sql file

---
+++ quasar-crud-project/database/users.sql
@@ -0,0 +1,23 @@
+-- Adminer 4.8.1 MySQL 5.5.5-10.6.16-MariaDB-0ubuntu0.22.04.1 dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+SET NAMES utf8mb4;
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(50) NOT NULL,
+  `user_age` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_turkish_ci;
+
+INSERT INTO `users` (`id`, `user_name`, `user_age`) VALUES
+(1,	'Kaan',	24),
+(2,	'Ahmet',	38)
+ON DUPLICATE KEY UPDATE `id` = VALUES(`id`), `user_name` = VALUES(`user_name`), `user_age` = VALUES(`user_age`);
+
+-- 2024-05-29 19:09:40
