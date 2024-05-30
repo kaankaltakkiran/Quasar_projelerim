e661e385ac176f3320064a133e407106efc9f57f
Kaan Kaltakkıran <durdu.kaan.kaltakkiran@gmail.com>
2024-05-30 16:00:55 +0300

    added for delete person record confirim dialog before delete

--- quasar-crud-project/src/pages/PersonsPage.vue
+++ quasar-crud-project/src/pages/PersonsPage.vue
@@ -20,15 +20,28 @@
       </template>
          <template v-slot:body-cell-delete="props">
         <q-td :props="props">
-          <q-btn color="negative" dense @click="deleteUser(props.row.id)">Delete</q-btn>
+       <q-btn color="negative" dense @click="confirmDeleteUser(props.row.id)">Delete User</q-btn>
         </q-td>
       </template>
     </q-table>
   </div>
+   <q-dialog v-model="confirm" medium persistent>
+      <q-card>
+        <q-card-section class="row items-center">
+          <span class="q-ml-sm">Kayıdı silmek istediğinizden emin misiniz ?</span>
+        </q-card-section>
+
+        <q-card-actions align="right">
+         <q-btn flat label="Hayır" color="red" @click="confirm = false" />
+         <!-- id null değilse silme işlemi çalışacak -->
+        <q-btn flat label="Evet" color="primary" @click="deleteUser(selectedUserId!)" />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import { ref,Ref, onMounted } from 'vue'
 import { QTableColumn } from 'quasar'
 import axios from 'axios'
 
@@ -49,6 +62,10 @@ const columns: QTableColumn[] = [
 const rows = ref<Person[]>([]);
 // tablo verilerini filtrele
 const filter = ref('');
+// silme işlemi için onay kutusu
+const confirm: Ref<boolean> = ref(false)
+// silinecek kullanıcının id'si
+const selectedUserId: Ref<number | null> = ref(null)
 
 const fetchData = async () => {
   try {
@@ -59,21 +76,29 @@ const fetchData = async () => {
     console.error('Error fetching data:', error)
   }
 }
+
+// silme işlemi için onay verilirse id'si verilen kullanıcıyı sil
+const confirmDeleteUser = (id: number) => {
+  selectedUserId.value = id
+  confirm.value = true
+}
 // id'si verilen kullanıcıyı sil
 const deleteUser = async (id: number) => {
   console.log('Deleting user:', id)
   try {
-    // Silinecek kullanıcının id'sini içeren JSON verisi gönderiliyor
+       // Silinecek kullanıcının id'sini içeren JSON verisi gönderiliyor
     //normal url de kabul etmedi
     //await axios.delete(`http://localhost/veri/crud-project/api.php?id=${id}`)
     //bu şekil de olmalı  await axios.delete('http://localhost/veri/crud-project/api.php', { params: { id: id } })
     await axios.delete('http://localhost/veri/crud-project/api.php', { data: { id: id } })
     rows.value = rows.value.filter((user) => user.id !== id)
     console.log('User deleted:', id)
+    confirm.value = false
   } catch (error) {
     console.error('Error deleting user:', error)
   }
 }
+
 // tablo verilerini sayfa yüklendiğinde çek
 onMounted(() => {
   fetchData()
