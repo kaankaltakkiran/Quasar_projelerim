6999b4da0e0c48d89bab0945d9d68995f6f06d95
Kaan Kaltakkıran <durdu.kaan.kaltakkiran@gmail.com>
2024-05-30 11:27:06 +0300

    added persons navbar link

--- quasar-crud-project/src/layouts/MainLayout.vue
+++ quasar-crud-project/src/layouts/MainLayout.vue
@@ -6,8 +6,16 @@
         <q-toolbar-title>
           Quasar App
         </q-toolbar-title>
-
+<q-tabs dense align="right" v-model="tab">
+  <q-route-tab
+   label="Persons"
+    name="persons"
+    to="/persons"
+    exact
+  />
+</q-tabs>
         <div>Quasar v{{ $q.version }}</div>
+        
       </q-toolbar>
     </q-header>
 
@@ -18,8 +26,10 @@
 </template>
 
 <script setup lang="ts">
-
+import {ref, Ref } from 'vue'
 defineOptions({
   name: 'MainLayout'
 });
+
+const tab: Ref<string> = ref('persons')
 </script>
