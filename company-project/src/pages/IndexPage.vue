19dd0faf75d35b63fc37968aac42035a40bf93ba
Kaan Kaltakkıran <durdu.kaan.kaltakkiran@gmail.com>
2024-06-07 20:21:39 +0300

    added person add with axios post  to database and show notify

--- company-project/src/pages/IndexPage.vue
+++ company-project/src/pages/IndexPage.vue
@@ -8,15 +8,15 @@
       <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
         <q-input
           filled
-          v-model="email"
-          label="Your email *"
+          v-model="name"
+          label="Your name *"
           lazy-rules
           :rules="[(val) => (val && val.length > 0) || 'Please type something']"
         />
         <q-input
           filled
-          v-model="name"
-          label="Your name *"
+          v-model="email"
+          label="Your email *"
           lazy-rules
           :rules="[(val) => (val && val.length > 0) || 'Please type something']"
         />
@@ -29,9 +29,7 @@
           dense
         >
         </q-option-group>
-
         <q-toggle v-model="accept" label="I accept the license and terms" />
-
         <div>
           <q-btn label="Submit" type="submit" color="primary" />
           <q-btn
@@ -50,14 +48,15 @@
 <script setup lang="ts">
 import { useQuasar } from 'quasar';
 import { ref, Ref } from 'vue';
+import axios from 'axios';
 
 defineOptions({ name: 'IndexPage' });
+//notify için
 const $q = useQuasar();
-
+//form değerlerini tanımla
 const email: Ref<string | null> = ref(null);
 const name: Ref<string | null> = ref(null);
 const accept: Ref<boolean> = ref(false);
-
 const status: Ref<string | null> = ref('0');
 
 const options = [
@@ -71,7 +70,8 @@ const options = [
   },
 ];
 
-const onSubmit = () => {
+//form submit edilmişse
+const onSubmit = async () => {
   if (!accept.value) {
     $q.notify({
       color: 'red-5',
@@ -80,18 +80,51 @@ const onSubmit = () => {
       message: 'You need to accept the license and terms first',
       position: 'top-right',
     });
-  } else {
+    return;
+  }
+  try {
+    //form verilerini apiye gönder
+    const response = await axios.post(
+      'http://localhost/veri/ornek_api/api.php',
+      {
+        email: email.value,
+        name: name.value,
+        status: status.value,
+        method: 'add-user',
+      }
+    );
+    // console.log('Response:', response);
+    //işlem başarılıysa
+    if (response.data.success === true) {
+      $q.notify({
+        color: 'green-4',
+        textColor: 'white',
+        icon: 'cloud_done',
+        message: 'User added successfully',
+        position: 'top-right',
+      });
+      onReset(); //formu sıfırla
+    } else {
+      $q.notify({
+        color: 'red-5',
+        textColor: 'white',
+        icon: 'warning',
+        message: 'An error occurred while sending the form',
+        position: 'top-right',
+      });
+    }
+  } catch (error) {
+    console.error('Error sending form:', error);
     $q.notify({
-      color: 'green-4',
+      color: 'red-5',
       textColor: 'white',
-      icon: 'cloud_done',
-      message: 'Submitted',
+      icon: 'warning',
+      message: 'An error occurred while sending the form',
       position: 'top-right',
     });
-    onReset();
   }
 };
-
+//formu sıfırla
 const onReset = () => {
   email.value = null;
   name.value = null;
