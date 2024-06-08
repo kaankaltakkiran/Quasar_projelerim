419bc4250a61a59b5bff183931ff564a52947f44
Kaan Kaltakkıran <durdu.kaan.kaltakkiran@gmail.com>
2024-06-08 22:59:26 +0300

    for person list page update person and set message possitive, negative and info

--- company-project/src/pages/PersonsPage.vue
+++ company-project/src/pages/PersonsPage.vue
@@ -1,6 +1,7 @@
 <template>
   <h2 class="text-red text-center">Person List</h2>
   <div class="row justify-center">
+    <!-- Table start-->
     <div class="q-pa-md col-12 col-sm-10 col-md-8 col-lg-6">
       <q-table
         flat
@@ -14,6 +15,7 @@
         v-model:selected="selected"
         :filter="filter"
       >
+        <!-- checkbox-->
         <template v-slot:body-cell-selection="props">
           <q-checkbox v-model="props.selected" />
         </template>
@@ -30,17 +32,55 @@
             </template>
           </q-input>
         </template>
-        <!-- Silme butonu -->
+        <!-- Delete butonu -->
         <template v-slot:top>
           <q-btn
             label="Delete Person"
             color="negative"
             @click="confirmDelete"
           />
+          <!-- Update butonu-->
+          <q-btn
+            class="q-ml-md"
+            label="Update Person"
+            color="primary"
+            @click="confirmUpdate"
+          />
         </template>
       </q-table>
+      <!-- Table end-->
       <div class="q-mt-md">Selected: {{ JSON.stringify(selected) }}</div>
     </div>
+    <!-- Güncelleme Dialogu -->
+    <q-dialog v-model="showUpdateDialog">
+      <q-card style="width: 700px; max-width: 80vw">
+        <q-card-section>
+          <q-input
+            class="q-mb-md"
+            v-model="selectedToUpdate.user_name"
+            label="Name"
+          />
+          <q-input
+            class="q-mb-md"
+            v-model="selectedToUpdate.user_email"
+            label="Email"
+          />
+          <q-input
+            class="q-mb-md"
+            v-model="selectedToUpdate.user_status"
+            label="Status"
+          />
+        </q-card-section>
+        <q-card-actions align="center">
+          <q-btn label="Update" color="primary" @click="updateSelectedRows" />
+          <q-btn
+            label="Cancel"
+            color="negative"
+            @click="showUpdateDialog = false"
+          />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
   </div>
 </template>
 
@@ -54,9 +94,17 @@ import { useQuasar } from 'quasar';
 const $q = useQuasar();
 
 // tablo değişkenleri
-const rows = ref<IPerson[]>([]);
+const rows = ref<IPerson[]>([]); // tablo verilerini tut
 const selected = ref<IPerson[]>([]); //seçilen verileri tut
-const filter = ref('');
+// güncellenmek istenen veriyi tut
+const selectedToUpdate = ref<IPerson>({
+  id: 0,
+  user_name: '',
+  user_email: '',
+  user_status: 0,
+});
+const filter = ref(''); // tablo filtresi
+const showUpdateDialog = ref(false); // güncelleme dialogunu göster
 
 // tablo sütunlarını belirle
 const columns: QTableColumn[] = [
@@ -92,89 +140,44 @@ const fetchData = async () => {
         method: 'get-users',
       }
     );
-    // console.log(response);
     if (response.data.success === true) {
       rows.value = response.data.users; // tablo verilerini doldur
     }
   } catch (error) {
     console.error('Error fetching data:', error);
+    triggerNotification('Error during data deletion!', 'negative');
   }
 };
 // seçilen satırları silme fonksiyonu
 const deleteSelectedRows = async () => {
   try {
-    // seçilen verilerin id'lerini al
     const selectedIds = selected.value.map((IPerson) => IPerson.id);
-    console.log(selectedIds);
     const response = await axios.post(
       'http://localhost/veri/ornek_api/api.php',
       {
         method: 'delete-user',
-        ids: selectedIds, // id parametresi bir dizi olarak gönderiliyor
+        ids: selectedIds, // ids parametresi bir dizi olarak gönderiliyor
       }
     );
-    console.log(response);
     if (response.data.success === true) {
       selected.value = []; // seçilen verileri temizle
       fetchData(); // verileri tekrar çek
-      triggerPositive(); // Başarılı silme işlemi sonrasında bildirim göster
+      triggerNotification('Selected users deleted successfully!', 'positive');
     }
   } catch (error) {
     console.error('Error deleting data:', error);
-    triggerNegative(); // Hata oluştuğunda bildirim göster
+    triggerNotification('Error during data deletion!', 'negative');
   }
-};
-// seçilen verileri string olarak döndür
-function getSelectedString() {
-  return selected.value.length === 0
-    ? ''
-    : `${selected.value.length} record${
-        selected.value.length > 1 ? 's' : ''
-      } selected of ${rows.value.length}`;
-}
-// sayfa yüklendiğinde verileri çek
-onMounted(() => {
-  fetchData();
-});
-const triggerPositive = () => {
-  $q.notify({
-    color: 'green-4',
-    textColor: 'white',
-    icon: 'cloud_done',
-    message: 'Person deleted successfully',
-    position: 'top-right',
-    timeout: 750,
-  });
-};
-const triggerNegative = () => {
-  $q.notify({
-    color: 'red-5',
-    textColor: 'white',
-    icon: 'warning',
-    message: 'error occurred when deleting the user !',
-    position: 'top-right',
-    timeout: 1000,
-  });
-};
-const triggerInfo = () => {
-  $q.notify({
-    color: 'blue-5',
-    textColor: 'white',
-    icon: 'info',
-    message: 'please delete the person you want to delete !',
-    position: 'top-right',
-    timeout: 1000,
-  });
 };
 // Delete onay kutusu
 const confirmDelete = () => {
   if (selected.value.length === 0) {
-    triggerInfo(); // Eğer hiçbir satır seçilmediyse bilgi mesajı göster
+    triggerInfo('Please select at least one user to delete !');
     return;
   }
   $q.dialog({
     title: 'Confirm',
-    message: 'Are you sure you want to delete the selected users?',
+    message: 'Are you sure you want to delete the selected users ?',
     cancel: true,
     persistent: true,
   })
@@ -187,5 +190,80 @@ const confirmDelete = () => {
     .onDismiss(() => {
       console.log('Dialog dismissed');
     });
+};
+// seçilen verileri string olarak döndür
+function getSelectedString() {
+  return selected.value.length === 0
+    ? ''
+    : `${selected.value.length} record${
+        selected.value.length > 1 ? 's' : ''
+      } selected of ${rows.value.length}`;
+}
+// seçilen veriyi güncelleme
+const updateSelectedRows = async () => {
+  try {
+    const response = await axios.post(
+      'http://localhost/veri/ornek_api/api.php',
+      {
+        method: 'update-user',
+        id: selectedToUpdate.value.id,
+        name: selectedToUpdate.value.user_name,
+        email: selectedToUpdate.value.user_email,
+        status: selectedToUpdate.value.user_status,
+      }
+    );
+    if (response.data.success === true) {
+      showUpdateDialog.value = false; // Güncelleme dialogunu kapat
+      fetchData(); // verileri tekrar çek
+      triggerNotification('Record updated successfully!', 'positive');
+    }
+  } catch (error) {
+    console.error('Error updating data:', error);
+    triggerNotification('Error during data update!', 'negative');
+  }
+};
+// Güncelleme Dialogu fonksiyonları
+const confirmUpdate = () => {
+  if (selected.value.length === 0) {
+    triggerInfo('Please select a record to update!'); // Hiçbir kayıt seçilmemişse bilgi mesajı göster
+    return;
+  }
+  if (selected.value.length !== 1) {
+    triggerInfo('You can update only one record!'); // Sadece bir kullanıcı seçilmemişse bilgi mesajı göster
+    return;
+  }
+  // Seçilen kullanıcının verilerini güncelleme dialogunda göstermek için seçilen verileri kopyala
+  selectedToUpdate.value = JSON.parse(JSON.stringify(selected.value[0]));
+  showUpdateDialog.value = true; // Güncelleme dialogunu göster
+};
+
+// sayfa yüklendiğinde verileri çek
+onMounted(() => {
+  fetchData();
+});
+//mesaj durumuna göre bildirim göster
+const triggerNotification = (
+  message: string,
+  type: 'positive' | 'negative'
+) => {
+  $q.notify({
+    color: type === 'positive' ? 'green-4' : 'red-5',
+    textColor: 'white',
+    icon: type === 'positive' ? 'cloud_done' : 'warning',
+    message,
+    position: 'top-right',
+    timeout: 1000,
+  });
+};
+//bilgi mesajı göster
+const triggerInfo = (message: string) => {
+  $q.notify({
+    color: 'blue-5',
+    textColor: 'white',
+    icon: 'info',
+    message,
+    position: 'top-right',
+    timeout: 1000,
+  });
 };
 </script>
