b61da6da51bcd19899e6e287719482dd0955b491
Kaan Kaltakkıran <durdu.kaan.kaltakkiran@gmail.com>
2024-06-11 17:44:14 +0300

    added info card component ad set responsive design

---
+++ ticket-project/src/components/InfoComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-card class="bg-secondary text-white">
+    <q-card-section>
+      <div class="text-h6">Our Changing Planet</div>
+      <div class="text-subtitle2">by John Doe</div>
+    </q-card-section>
+    <q-separator />
+    <q-card-section>
+      {{ lorem }}
+    </q-card-section>
+
+    <q-separator dark />
+
+    <q-card-actions>
+      <q-btn flat>Action 1</q-btn>
+      <q-btn flat>Action 2</q-btn>
+    </q-card-actions>
+  </q-card>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const lorem = ref(
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+);
+</script>
+
+<style lang="sass" scoped></style>
--- ticket-project/src/pages/PaymentPage.vue
+++ ticket-project/src/pages/PaymentPage.vue
@@ -1,11 +1,25 @@
 <template>
-  <div>
-    <h1>Payment Page</h1>
-    <p>Payment Page Content</p>
+  <div class="q-mt-md row justify-center q-gutter-x-md">
+    <!-- 1. Satır -->
+    <div class="col-lg-3 col-md-6 col-12 q-pa-md">
+      <InfoComponent></InfoComponent>
+    </div>
+    <div class="col-lg-3 col-md-6 col-12 q-pa-md">
+      <InfoComponent></InfoComponent>
+    </div>
+    <div class="col-lg-3 col-md-6 col-12 q-pa-md">
+      <InfoComponent></InfoComponent>
+    </div>
+  </div>
+  <!-- 2. Satır -->
+  <div class="q-mt-md row justify-center q-gutter-x-md">
+    <div class="col-lg-3 col-md-6 col-12 q-pa-md">
+      <InfoComponent></InfoComponent>
+    </div>
   </div>
 </template>
-
 <script setup lang="ts">
+import InfoComponent from 'src/components/InfoComponent.vue';
 defineOptions({
   name: 'PaymentPage',
 });
