9033775c2b9856e3bb7041f3d848f80bc636d22e
Kaan Kaltakkıran <durdu.kaan.kaltakkiran@gmail.com>
2024-06-12 17:27:17 +0300

    added calaender set today choose and set priave day set colour teal

--- ticket-project/src/components/CalendarComponent.vue
+++ ticket-project/src/components/CalendarComponent.vue
@@ -1,21 +1,45 @@
 <template>
   <div class="row justify-center">
     <div class="col-lg-3 col-md-6 col-12 q-pa-md">
-      <q-date v-model="date" :events="events" />
+      <q-date
+        v-model="date"
+        :events="eventDates"
+        :event-color="getEventColor"
+        mask="DD/MM/YYYY"
+      />
+      {{ date }}
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, Ref } from 'vue';
+import { ref } from 'vue';
 
-const date: Ref<string> = ref('2019/02/01');
+const currentDate = new Date();
+const formattedCurrentDate = `${currentDate
+  .getDate()
+  .toString()
+  .padStart(2, '0')}/${(currentDate.getMonth() + 1)
+  .toString()
+  .padStart(2, '0')}/${currentDate.getFullYear()}`;
+const date = ref(formattedCurrentDate);
 
 const events = [
-  '2019/02/01',
-  '2019/02/05',
-  '2019/02/06',
-  '2019/02/09',
-  '2019/02/23',
+  {
+    date: '2024/06/11',
+    label: 'İstanbul - Ankara',
+  },
 ];
+
+// Extract event dates
+const eventDates = events.map((event) => event.date);
+
+// Event color getter function
+const getEventColor = (date: string): string => {
+  const selectedDate = new Date(date);
+  if (selectedDate.getTime() < currentDate.getTime()) {
+    return 'teal';
+  }
+  return '';
+};
 </script>
