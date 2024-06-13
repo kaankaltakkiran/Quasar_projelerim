fa0725d13727dd26e98319edc634803e7e34a927
Kaan Kaltakkıran <durdu.kaan.kaltakkiran@gmail.com>
2024-06-13 21:15:46 +0300

    updateded info component

--- ticket-project/src/components/InfoComponent.vue
+++ ticket-project/src/components/InfoComponent.vue
@@ -1,61 +1,66 @@
 <template>
-  <q-card class="q-pa-md">
-    <q-card-section class="q-gutter-md">
-      <div class="row items-center">
-        <div style="flex-shrink: 0">
+  <q-card>
+    <q-list bordered class="rounded-borders">
+      <q-item>
+        <q-item-section>
           <q-avatar
-            :icon="props.info.icon"
+            icon="directions_bus"
             text-color="white"
             color="primary"
             class="q-ml-md"
           />
-        </div>
-        <div style="flex-grow: 1">
-          <div class="text-h6 text-center">Metro Turizm</div>
-        </div>
-      </div>
-    </q-card-section>
+        </q-item-section>
+        <!--Side:yana al-->
+        <q-item-section side>
+          <q-item-label header class="text-caption">Metro Turizm </q-item-label>
+        </q-item-section>
+      </q-item>
 
-    <q-separator />
-    <q-card-section class="q-gutter-md">
-      <div class="row items-center justify-between">
-        <div>
-          <div>Kalkış:</div>
-          <div>Varış:</div>
-          <div>Otogarlar:</div>
-          <div>Hareket Zamanı:</div>
-          <div>Koltuk:</div>
-        </div>
-        <div>
-          <div>{{ props.info.departure }}</div>
-          <div>{{ props.info.arrival }}</div>
-          <div>{{ props.info.date }}</div>
-          <div>{{ props.info.time }}</div>
-          <div>{{ props.info.seat }}</div>
-        </div>
-      </div>
-    </q-card-section>
-    <q-separator />
-    <q-card-section class="q-gutter-md">
-      <div class="text-h6">Bilet İşlemleri</div>
-      <div>{{ props.info.ticketInfo }}</div>
-    </q-card-section>
+      <q-separator />
 
-    <q-separator dark />
+      <q-item>
+        <q-item-section>
+          <q-item-label>Kalkış:</q-item-label>
+          <q-item-label caption>İstanbul</q-item-label>
+        </q-item-section>
 
-    <q-card-actions>
-      <div class="text-subtitle2 text-red">
-        {{ props.info.notice }}
-      </div>
-    </q-card-actions>
+        <q-item-section>
+          <q-item-label>Varış:</q-item-label>
+          <q-item-label caption>Ankara</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-item>
+        <q-item-section>
+          <q-item-label>Hareket Zamanı:</q-item-label>
+          <q-item-label caption>15 Haziran Perşembe,Saat: 02:00</q-item-label>
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>Koltuk No:</q-item-label>
+          <q-item-label caption>1,2,3</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-item>
+        <q-item-section>
+          <q-item-label>Bilet İşlemleri:</q-item-label>
+          <q-item-label caption
+            >Seferin 6 saat öncesine kadar biletinizi iptal edebilir ve
+            değiştirebilirsiniz. Biletiniz açığa alınamaz.
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-item>
+        <q-item-section>
+          <q-item-label>Uyarı:</q-item-label>
+          <q-item-label caption
+            >Otobüs,Perşembe'yi Cuma'ya bağlayan gece hareket edecektir.
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-card>
 </template>
 
-<script setup lang="ts">
-import { defineProps } from 'vue';
-import { ITravelInfo } from 'components/models';
-
-const props = defineProps<{
-  info: ITravelInfo;
-}>();
-</script>
+<script setup lang="ts"></script>
--- ticket-project/src/pages/PaymentPage.vue
+++ ticket-project/src/pages/PaymentPage.vue
@@ -2,20 +2,7 @@
   <div class="q-mt-md row justify-center q-gutter-x-md">
     <!-- 1. Satır -->
     <div class="col-lg-3 col-md-6 col-12 q-pa-md">
-      <InfoComponent
-        :info="{
-          icon: 'directions_bus',
-          title: 'Şirket 1',
-          departure: 'İstanbul',
-          arrival: 'Ankara',
-          date: '15 Haziran Perşembe',
-          time: '12:00',
-          seat: '24A',
-          ticketInfo:
-            'Seferin 6 saat öncesine kadar biletinizi iptal edebilir ve değiştirebilirsiniz. Biletiniz açığa alınamaz.',
-          notice: 'Otobüs Perşembe günü kalkacaktır.',
-        }"
-      />
+      <InfoComponent />
     </div>
     <div class="col-lg-3 col-md-6 col-12 q-pa-md">
       <ContactComponent />
