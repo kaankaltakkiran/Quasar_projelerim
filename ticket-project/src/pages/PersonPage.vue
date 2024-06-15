f7614c84fa96c1ec2293ed6589e021ca0a3eaf53
Kaan Kaltakkıran <durdu.kaan.kaltakkiran@gmail.com>
2024-06-15 20:37:27 +0300

    created baseurl axios and for person page add data from database

--- ticket-project/src/pages/PersonPage.vue
+++ ticket-project/src/pages/PersonPage.vue
@@ -4,34 +4,38 @@
       <div class="col-xs-12 col-sm-6 col-md-2">
         <q-input
           filled
-          v-model="name1"
+          v-model="name"
           label="Your name *"
-          hint="Name and surname"
+          hint="Name "
           lazy-rules
-          :rules="[(val) => (val && val.length > 0) || 'Please type something']"
+          :rules="[(val) => (val && val.length > 0) || 'Boş bırakılamaz']"
         />
       </div>
       <div class="col-xs-12 col-sm-6 col-md-2">
         <q-input
           filled
-          v-model="name2"
-          label="Your name *"
-          hint="Name and surname"
+          v-model="email"
+          label="Email *"
+          hint="Email adresinizi giriniz"
           lazy-rules
-          :rules="[(val) => (val && val.length > 0) || 'Please type something']"
+          :rules="emailRules"
         />
       </div>
       <div class="col-xs-12 col-sm-6 col-md-2">
-        <q-input
+        <q-select
+          label="Kullanıcı Durumu Seçiniz *"
+          transition-show="flip-up"
+          transition-hide="flip-down"
           filled
-          v-model="name3"
-          label="Your name *"
-          hint="Name and surname"
-          lazy-rules
-          :rules="[(val) => (val && val.length > 0) || 'Please type something']"
+          v-model="status"
+          :options="options"
+          style="width: 250px"
+          :rules="[(val) => !!val || 'Please select a status']"
         />
       </div>
-      <q-toggle v-model="accept" label="I accept the license and terms" />
+      <div class="col-xs-12 col-sm-6 col-md-2">
+        <q-toggle v-model="accept" label="I accept the license and terms" />
+      </div>
       <div class="col-xs-12 col-sm-6 col-md-2 q-mt-md">
         <q-btn label="Submit" type="submit" color="primary" />
       </div>
@@ -72,39 +76,94 @@
 import { useQuasar } from 'quasar';
 import { ref } from 'vue';
 import { QTableColumn } from 'quasar';
+import { api } from 'boot/axios'; //axios instance
 
 const $q = useQuasar();
-
-const name1 = ref<string | null>(null);
-const name2 = ref<string | null>(null);
-const name3 = ref<string | null>(null);
+//form verileri
+const filter = ref('');
+const name = ref<string | null>(null);
+const email = ref<string | null>(null);
 const accept = ref<boolean>(false);
+const status = ref<string[]>([]);
 
-const onSubmit = () => {
+//select options
+const options = [
+  {
+    label: 'Pasif',
+    value: '0',
+  },
+  {
+    label: 'Aktif',
+    value: '1',
+  },
+];
+//email için kurallar
+const emailRules = [
+  (val: string) => !!val || 'Lütfen email adresinizi giriniz',
+  (val: string) => {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(val) || 'Geçersiz email adresi';
+  },
+];
+
+//form submit edilmişse
+const onSubmit = async () => {
   if (!accept.value) {
     $q.notify({
       color: 'red-5',
       textColor: 'white',
       icon: 'warning',
       message: 'You need to accept the license and terms first',
+      position: 'top-right',
     });
-  } else {
+    return;
+  }
+  try {
+    //form verilerini apiye gönder
+    const response = await api.post('api.php', {
+      name: name.value,
+      email: email.value,
+      status: status.value,
+      method: 'add-user',
+    });
+    console.log('Response:', response);
+    //işlem başarılıysa
+    if (response.data.success === true) {
+      $q.notify({
+        color: 'green-4',
+        textColor: 'white',
+        icon: 'cloud_done',
+        message: 'User added successfully',
+        position: 'top-right',
+      });
+      onReset(); //formu sıfırla
+    } else {
+      $q.notify({
+        color: 'red-5',
+        textColor: 'white',
+        icon: 'warning',
+        message: 'An error occurred while sending the form',
+        position: 'top-right',
+      });
+    }
+  } catch (error) {
+    console.error('Error sending form:', error);
     $q.notify({
-      color: 'green-4',
+      color: 'red-5',
       textColor: 'white',
-      icon: 'cloud_done',
-      message: 'Submitted',
+      icon: 'warning',
+      message: 'An error occurred while sending the form',
+      position: 'top-right',
     });
   }
 };
-
+//formu sıfırla
 const onReset = () => {
-  name1.value = null;
-  name2.value = null;
-  name3.value = null;
+  name.value = null;
+  email.value = null;
   accept.value = false;
+  status.value = [];
 };
-
 interface Row {
   name: string;
   calories: number;
@@ -193,5 +252,4 @@ const rows: Row[] = [
     carbs: 65,
   },
 ];
-const filter = ref('');
 </script>
