fbc33e89a5c067c32eed761980aab92e4960a1f1
Kaan Kaltakkıran <durdu.kaan.kaltakkiran@gmail.com>
2024-06-27 17:02:29 +0300

    added basic travel info component

---
+++ ticket-project/src/components/TravelInfoComponent.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>Travel Info</h1>
+</template>
+
+<script setup></script>
--- ticket-project/src/pages/IndexPage.vue
+++ ticket-project/src/pages/IndexPage.vue
@@ -2,13 +2,11 @@
   <div class="q-mt-md row justify-center">
     <!-- 1. Satır -->
     <div class="col-lg-3 col-md-6 col-12 q-pa-md">
-      <h2 class="text-center">Index Page</h2>
+      <TravelInfoComponent> </TravelInfoComponent>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-defineOptions({
-  name: 'IndexPage',
-});
+import TravelInfoComponent from '../components/TravelInfoComponent.vue';
 </script>
