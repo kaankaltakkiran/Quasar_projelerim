ea2782f83cf44c7ea46795354ce7d9a0baad25ef
Kaan Kaltakkıran <durdu.kaan.kaltakkiran@gmail.com>
2024-06-27 17:52:14 +0300

    added basic for index page travel info

--- ticket-project/src/pages/IndexPage.vue
+++ ticket-project/src/pages/IndexPage.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="q-mt-md row justify-center">
-    <!-- 1. Satır -->
     <div class="col-lg-3 col-md-6 col-12 q-pa-md">
       <TravelInfoComponent> </TravelInfoComponent>
     </div>
