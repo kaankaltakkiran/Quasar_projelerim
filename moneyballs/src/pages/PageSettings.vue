e30a1d29a0677bb165959c03b289cf72fab5cc74
Kaan Kaltakkıran <durdu.kaan.kaltakkiran@gmail.com>
2024-06-30 13:00:56 +0300

    proje set entires page and settings page layout,page and navigation

---
+++ moneyballs/src/pages/PageSettings.vue
@@ -0,0 +1,7 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <p>Settings Page</p>
+  </q-page>
+</template>
+
+<script setup lang="ts"></script>
--- moneyballs/src/router/routes.ts
+++ moneyballs/src/router/routes.ts
@@ -4,7 +4,13 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [
+      { path: '', component: () => import('src/pages/PageEntries.vue') },
+      {
+        path: 'settings',
+        component: () => import('src/pages/PageSettings.vue'),
+      },
+    ],
   },
 
   // Always leave this as last one,
