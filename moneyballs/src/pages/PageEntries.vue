15e0aa627e68e499bde31c1e802839241811a3a4
Kaan Kaltakkıran <durdu.kaan.kaltakkiran@gmail.com>
2024-06-30 19:12:52 +0300

    added delete item with confirm dialog

--- moneyballs/src/pages/PageEntries.vue
+++ moneyballs/src/pages/PageEntries.vue
@@ -2,23 +2,36 @@
   <q-page>
     <div class="q-pa-md">
       <q-list bordered separator>
-        <q-item v-for="entry in entries" :key="entry.id">
-          <q-item-section
-            class="text-weight-bold"
-            :class="useAmountColorClass(entry.amount)"
-          >
-            {{ entry.name }}
-          </q-item-section>
-          <q-item-section
-            class="text-weight-bold"
-            side
-            :class="useAmountColorClass(entry.amount)"
-          >
-            {{ useCurrencify(entry.amount) }}
-          </q-item-section>
-        </q-item>
+        <q-slide-item
+          v-for="entry in entries"
+          :key="entry.id"
+          @right="onEntrySlideRight($event, entry.id)"
+          left-color="positive"
+          right-color="negative"
+        >
+          <template v-slot:right>
+            <q-icon name="delete" />
+          </template>
+
+          <q-item>
+            <q-item-section
+              class="text-weight-bold"
+              :class="useAmountColorClass(entry.amount)"
+            >
+              {{ entry.name }}
+            </q-item-section>
+            <q-item-section
+              class="text-weight-bold"
+              side
+              :class="useAmountColorClass(entry.amount)"
+            >
+              {{ useCurrencify(entry.amount) }}
+            </q-item-section>
+          </q-item>
+        </q-slide-item>
       </q-list>
     </div>
+    <!-- Footer Section -->
     <q-footer class="bg-transparent">
       <div class="row q-mb-sm q-px-md q-my-sm shadow-up-3">
         <div class="col text-grey-7 text-h6">Balance:</div>
@@ -29,6 +42,7 @@
           {{ useCurrencify(balance) }}
         </div>
       </div>
+      <!-- Form Section -->
       <q-form
         @submit="addEntry"
         class="row q-px-sm q-pb-sm q-col-gutter-sm bg-primary"
@@ -64,10 +78,13 @@
 
 <script setup lang="ts">
 import { ref, computed, reactive } from 'vue';
-import { uid } from 'quasar';
+import { uid, useQuasar } from 'quasar';
 // use importları
 import { useCurrencify } from 'src/use/useCurrencify';
 import { useAmountColorClass } from 'src/use/useAmountColorClass';
+
+// useQuasar fonksiyonu, Quasar fonksiyonlarını kullanmamızı sağlar
+const $q = useQuasar();
 
 const entries = ref([
   {
@@ -135,5 +152,42 @@ const addEntry = () => {
 
   entries.value.push(newEntry); // yeni girdiyi ekle
   addEntryFormReset(); // formu sıfırla
+};
+/* Silme işlemi */
+
+const onEntrySlideRight: (
+  { reset }: { reset: () => void },
+  entryId: string // entryId, silinecek girdinin id'sini temsil eder
+) => void = ({ reset }, entryId) => {
+  $q.dialog({
+    title: 'Delete Entry',
+    message: 'Are you sure you want to delete this entry?',
+    cancel: {
+      color: 'primary',
+      noCaps: true,
+    },
+    persistent: true,
+    ok: {
+      label: 'delete',
+      color: 'negative',
+      noCaps: true,
+    },
+  })
+    .onOk(() => {
+      // console.log('>>>> OK')
+      deleteEntry(entryId);
+    })
+    .onCancel(() => {
+      // console.log('>>>> Cancel')
+      reset(); // Call the reset function
+    });
+};
+
+/* Silme fonksiyonu */
+
+const deleteEntry = (entryId: string) => {
+  //console.log('Deleting entry with id:', entryId);
+  const index = entries.value.findIndex((entry) => entry.id === entryId); // entryId'ye göre girdinin index'ini bul
+  entries.value.splice(index, 1); // girdiyi sil
 };
 </script>
