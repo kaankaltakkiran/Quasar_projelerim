a6ed9e6d1085c2c994840a8b0698222d265bea1e
Kaan Kaltakkıran <durdu.kaan.kaltakkiran@gmail.com>
2024-08-30 12:09:52 +0300

    created diffrent lang and select option

--- quasalang/src/pages/IndexPage.vue
+++ quasalang/src/pages/IndexPage.vue
@@ -1,15 +1,34 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page padding>
+    <q-select
+      class="q-mb-md"
+      v-model="locale"
+      :options="localeOptions"
+      label="Quasar Language"
+      dense
+      borderless
+      emit-value
+      map-options
+      options-dense
+      filled
+      style="min-width: 150px"
+    />
+    <q-btn :label="$t('hello')"></q-btn>
+    <q-btn :label="$t('goodbye')"></q-btn>
+    <q-btn :label="$t('thanks')"></q-btn>
+    <q-btn :label="$t('buttHair')"></q-btn>
   </q-page>
 </template>
 
 <script setup>
-defineOptions({
-  name: 'IndexPage'
-});
+import { useI18n } from "vue-i18n";
+
+const { locale } = useI18n({ useScope: "global" });
+
+const localeOptions = [
+  { value: "en-US", label: "English" },
+  { value: "de", label: "German" },
+  { value: "fr", label: "French" },
+  { value: "it", label: "Italian" },
+];
 </script>
