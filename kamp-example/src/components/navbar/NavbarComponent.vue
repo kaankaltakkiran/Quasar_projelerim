1a7d91ccce98d2ae1913eb7fd9c26cd57cedacb0
Kaan Kaltakkıran <durdu.kaan.kaltakkiran@gmail.com>
2025-07-30 18:32:28 +0300

    navbar set component

---
+++ kamp-example/src/components/navbar/NavbarComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-toolbar class="bg-primary text-white shadow-2 rounded-borders">
+    <!--  <q-btn flat label="E-ticaret" /> -->
+
+    <q-toolbar-title> Quasar App </q-toolbar-title>
+    <q-space />
+
+    <q-tabs v-model="tab" shrink>
+      <q-tab name="home" label="Ana Sayfa" />
+      <q-tab name="products" label="Ürünler" />
+      <q-tab name="register" label="Üye Ol" />
+      <q-tab name="login" label="Giriş Yap" />
+      <q-tab name="logout" label="Çıkış Yap" />
+    </q-tabs>
+  </q-toolbar>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const tab = ref<string>('home');
+</script>
--- kamp-example/src/layouts/MainLayout.vue
+++ kamp-example/src/layouts/MainLayout.vue
@@ -1,18 +1,7 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
-      <q-toolbar class="bg-primary text-white shadow-2 rounded-borders">
-        <q-btn flat label="E-ticaret" />
-        <q-space />
-
-        <q-tabs v-model="tab" shrink>
-          <q-tab name="home" label="Ana Sayfa" />
-          <q-tab name="products" label="Ürünler" />
-          <q-tab name="register" label="Üye Ol" />
-          <q-tab name="login" label="Giriş Yap" />
-          <q-tab name="logout" label="Çıkış Yap" />
-        </q-tabs>
-      </q-toolbar>
+      <NavbarComponent />
     </q-header>
 
     <q-page-container>
@@ -22,7 +11,5 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-
-const tab = ref<string>('home');
+import NavbarComponent from 'components/navbar/NavbarComponent.vue';
 </script>
